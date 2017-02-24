8ad2da99891be39354f19521ede7e1a555dfb2fd
Łukasz Księżak <lukasz.ksiezak@interia.pl>
2017-02-24 21:31:10 +0100

    Added facebook login startup class

---
+++ SocialMediaLoginApp/App_Start/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SocialMediaLoginApp.App_Start.Startup))]
+
+namespace SocialMediaLoginApp.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new Microsoft.Owin.Security.Cookies.CookieAuthenticationOptions()
+            {
+                AuthenticationType = "",
+                LoginPath = new PathString("/Auth/Login")
+            });
+            app.UseFacebookAuthentication(new Microsoft.Owin.Security.Facebook.FacebookAuthenticationOptions()
+            {
+                AppId = "",
+                AppSecret = "",
+                SignInAsAuthenticationType = ""
+            });
+        }
+    }
+}
