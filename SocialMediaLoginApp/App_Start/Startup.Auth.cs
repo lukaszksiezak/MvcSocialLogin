365ef87aa2ceaf5eaff676ef3483e85b9b452a4a
Łukasz Księżak <lukasz.ksiezak@interia.pl>
2017-02-28 20:49:57 +0100

    Fix to Twitter authentication

--- SocialMediaLoginApp/App_Start/Startup.Auth.cs
+++ SocialMediaLoginApp/App_Start/Startup.Auth.cs
@@ -54,9 +54,25 @@ namespace LoginTemplate
             var consKey = ConfigurationManager.AppSettings["TwitterConsumerKey"];
             var consSec = ConfigurationManager.AppSettings["TwitterSecret"];
 
-            app.UseTwitterAuthentication(
-                consumerKey: consKey,
-                consumerSecret: consSec);
+            app.UseTwitterAuthentication(new Microsoft.Owin.Security.Twitter.TwitterAuthenticationOptions {
+                ConsumerKey = consKey,
+                ConsumerSecret = consSec,
+                BackchannelCertificateValidator = new Microsoft.Owin.Security.CertificateSubjectKeyIdentifierValidator(new[]
+                {
+                    "A5EF0B11CEC04103A34A659048B21CE0572D7D47", // VeriSign Class 3 Secure Server CA - G2
+                    "0D445C165344C1827E1D20AB25F40163D8BE79A5", // VeriSign Class 3 Secure Server CA - G3
+                    "7FD365A7C2DDECBBF03009F34339FA02AF333133", // VeriSign Class 3 Public Primary Certification Authority - G5
+                    "39A55D933676616E73A761DFA16A7E59CDE66FAD", // Symantec Class 3 Secure Server CA - G4
+                    "‎add53f6680fe66e383cbac3e60922e3b4c412bed", // Symantec Class 3 EV SSL CA - G3
+                    "4eb6d578499b1ccf5f581ead56be3d9b6744a5e5", // VeriSign Class 3 Primary CA - G5
+                    "01C3968ACDBD57AE7DFAFF9552311608CF23A9F9", // DigiCert SHA2 High Assurance Server C‎A 
+                    "B13EC36903F8BF4701D498261A0802EF63642BC3" // DigiCert High Assurance EV Root CA
+                })
+            });
+            //Important note to make Twitter authentication working:
+            //1) https://apps.twitter.com/
+            //2) Go to the Settings Tab
+            //3) Set a CallBack URL to any website. Even if it is not real.
 
             var fbId = ConfigurationManager.AppSettings["FacebookAppId"];
             var fbSec = ConfigurationManager.AppSettings["FacebookSecret"];
